fe892ef7f31a09dd6fd74cfd0a5d37d66049809e
Horeb Silva <horeb.silva241004@gmail.com>
2023-10-25 12:38:00 +0200

    td4

--- SQL & Prog/TD4/td4.sql
+++ SQL & Prog/TD4/td4.sql
@@ -29,5 +29,25 @@ EXECUTE FUNCTION inserDateEdit();
 
 -- b
 
-CREATE OR REPLACE FUNCTION codeEditionISBN() RETURNS TRIGGER as $$
+CREATE OR REPLACE FUNCTION verifCodeEditionISBN() RETURNS TRIGGER AS $$
 BEGIN
+    -- Convertir l'ISBN13 en chaîne de caractères
+    DECLARE isbnString VARCHAR := CAST(NEW.isbn AS VARCHAR);
+    
+    -- Extraire le code de l'éditeur à partir de l'ISBN
+    DECLARE extractedCode INTEGER := CAST(SPLIT_PART(isbnString, '-', 2) AS INTEGER);
+    
+    -- Vérifier si le code extrait correspond au code de la collection du livre
+    IF extractedCode != NEW.code_collection THEN
+        RAISE EXCEPTION 'Le code de la collection du livre ne correspond pas au code de l’éditeur dans l’ISBN.';
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Créer un trigger pour exécuter la fonction avant chaque insertion ou mise à jour sur la table des livres
+CREATE TRIGGER checkCodeEditionISBN
+BEFORE INSERT OR UPDATE ON bookbay.livres
+FOR EACH ROW
+EXECUTE FUNCTION verifCodeEditionISBN();
