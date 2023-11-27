af2199a64e56c3b9bb5c815ac2833ca37f370407
Horeb Silva <horeb.silva241004@gmail.com>
2023-11-27 09:12:25 +0100

    tp6 java complet

---
+++ Java/Tp6/exo3/Dessin.java
@@ -0,0 +1,26 @@
+package clubSport.exo3;
+
+//Composite
+public class Dessin extends Forme {
+    private ArrayList<Forme> formes;
+
+    public Dessin() {
+        this.formes = new ArrayList<Forme>();
+        }
+
+    public void add(Forme f){
+        formes.add(f);
+    }
+
+    public void translate ( int dx, int dy){
+            for (Forme f : formes){
+                f.translate (dx,dy);
+            }
+    }
+
+    public void dessine (){
+            for (Forme f : formes){
+                f.dessine();
+            }
+        }
+}
